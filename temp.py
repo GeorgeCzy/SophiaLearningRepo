59b84f4745c07986cfb7a2add1098a6560e1ba7f
RayCheng1130 <zcheng278@wisc.edu>
2026-02-03 13:30:49 +0800

    reset limit

---
+++ temp.py
@@ -0,0 +1,42 @@
+LIMITS = {
+    # =========================
+    # RIGHT ARM
+    # =========================
+    "RightShoulderPitch": (deg(-145), deg(35)),
+    "RightShoulderRoll":  (deg(-4),   deg(101)),
+    "RightShoulderYaw":   (deg(-66),  deg(83)),
+    "RightElbowPitch":    (deg(-127), deg(119)),
+    "RightElbowYaw":      (deg(-123), deg(123)),
+
+    # =========================
+    # RIGHT HAND
+    # =========================
+    "RightWristPitch":   (deg(-35),  deg(35)),
+    "RightWristRoll":    (deg(-35),  deg(35)),
+    "RightThumbRoll":    (deg(-31),  deg(22)),
+    "RightThumbFinger":  (deg(-75),  deg(44)),
+    "RightIndexFinger":  (deg(-123), deg(18)),
+    "RightMiddleFinger": (deg(-18),  deg(132)),
+    "RightRingFinger":   (deg(-18),  deg(136)),
+    "RightPinkyFinger":  (deg(-75),  deg(4)),
+    # =========================
+    # LEFT ARM
+    # =========================
+    "LeftShoulderPitch": (deg(-35),  deg(145)),
+    "LeftShoulderRoll":  (deg(-101), deg(4)),
+    "LeftShoulderYaw":   (deg(-83),  deg(66)),
+    "LeftElbowPitch":    (deg(-119), deg(127)),
+    "LeftElbowYaw":      (deg(-123), deg(123)),
+
+    # =========================
+    # LEFT HAND
+    # =========================
+    "LeftWristPitch":   (deg(-35),  deg(35)),
+    "LeftWristRoll":    (deg(-35),  deg(35)),
+    "LeftThumbRoll":    (deg(-31),  deg(22)),
+    "LeftThumbFinger":  (deg(-44),  deg(75)),
+    "LeftIndexFinger":  (deg(-18),  deg(123)),
+    "LeftMiddleFinger": (deg(-18),  deg(132)),
+    "LeftRingFinger":   (deg(-18),  deg(136)),
+    "LeftPinkyFinger":  (deg(-4),   deg(75)),
+}
